4acc870657c9ecfc501599a8f9356257870e6818
umurutasate3 <claudeumurutasate4@gmail.com>
2024-10-12 16:20:15 +0200

    hghkb

--- sms/sendSMS.js
+++ sms/sendSMS.js
@@ -3,23 +3,21 @@ const AfricasTalking = require('africastalking');
 // TODO: Initialize Africa's Talking
 
 const africastalking = AfricasTalking({
-    apiKey: 'atsk_d6ba7ffae0d99ba30acc28d795369eac314db7de2e359e890f7e5315cd339b556a19ef76',
-    username:'sandbox'
+    apiKey:'atsk_d6ba7ffae0d99ba30acc28d795369eac314db7de2e359e890f7e5315cd339b556a19ef76',
+    username: 'sandbox'
 });
 
 module.exports = async function sendSMS(to,message) {
     
     // TODO: Send message
-
 try {
   const result=await africastalking.SMS.send({
     to: to, 
     message: message,
-    from: 'Kagarama Cell'
+    from: ''
   });
   console.log(result);
 } catch(ex) {
   console.error(ex);
-}    
-
+}
 };
